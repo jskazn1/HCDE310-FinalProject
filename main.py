142635d4a7dd110e3c35344fb734b38a8d44fc8f
QIEN LIN <qienlin@D-10-18-208-39.dhcp4.washington.edu>
2018-12-06 18:17:24 -0800

    Draft1

--- main.py
+++ main.py
@@ -1,13 +1,47 @@
 from flask import Flask
 from flask import render_template
+from flask import request
+import json
 import os
+import urllib.parse
+import urllib.request
+
+base_url = 'https://api.meetup.com'
+api_key = '55422e26d4d4459537924533f116b25'
+
+def meetup_api(method, params=None):
+    parameters = params if params is not None else {}
+    parameters.update({
+        'key': api_key,
+        'sign': 'true'
+    })
+    url_params = urllib.parse.urlencode(parameters)
+    request_url = '{}{}?{}'.format(base_url, method, url_params)
+    print(request_url)
+    with urllib.request.urlopen(request_url) as response:
+        raw_data = response.read()
+    data = json.loads(raw_data)
+    return data
+
 
 app = Flask(__name__)
 
-@app.route("/")
+
+@app.route('/')
 def homepage():
-    return render_template("home.html")
+    return render_template('home.html', title='Our App Name')
 
 
-if __name__ == "__main__":
-    app.run(host = "127.0.0.1", port = 8080, debug = True)
+@app.route('/step-one', strict_slashes=False)
+def step_one():
+    state = request.args.get('state')
+    cities = meetup_api('/2/cities', params={
+        'country': 'US',
+        'state': state
+    })
+    return render_template('step-one.html', title='Searching {} - Bon Voyage'.format(state), state=state,
+                           cities=cities.get('results', []))
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
