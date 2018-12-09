2fd69c06518de20152a84c2301ff7575a73b4b2a
QIEN LIN <qienlin@QIENdeMacBook-Pro.local>
2018-12-08 18:21:57 -0800

    finished user input and getting results in json format

--- main.py
+++ main.py
@@ -29,9 +29,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    return render_template('home.html', title='Our App Name')
+    return render_template('home.html', title='Bon Voyage')
 
-
+# get request to the python server (Flask)
 @app.route('/step-one', strict_slashes=False)
 def step_one():
     state = request.args.get('state')
@@ -42,6 +42,28 @@ def step_one():
     return render_template('step-one.html', title='Searching {} - Bon Voyage'.format(state), state=state,
                            cities=cities.get('results', []))
 
+@app.route('/results', strict_slashes=False)
+# YYYY-MM-DDTHH:MM:SS.
+# 01/09/2018
+def results():
+    city = request.args.get('city').split('+')
+    date = request.args.get('daterange').split(" - ")
+    startDate = date[0]
+    startDate = ("{}-{}-{}{}").format(startDate[6:10], startDate[0:2], startDate[3:5], "T00:00:00")
+    endDate = date[1]
+    endDate = ("{}-{}-{}{}").format(endDate[6:10], endDate[0:2], endDate[3:5], "T00:00:00")
+    event = meetup_api('/find/upcoming_events', params={
+       'end_date_range': endDate,
+       'start_date_range': startDate,
+       'lat': city[1],
+       'lon': city[2],
+       'radius': '100',
+       'page': "10"
+    })
+    return render_template('results.html', title='Searching {} - Bon Voyage'.format(city), city=city,
+                          event=event.get('results', []))
+
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
+
